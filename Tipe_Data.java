a3e19d518814c41655a99132a844eff2e20d0056
danilfatah-ai <fatahbiawak655@gmail.com>
2026-03-10 09:51:39 +0700

    Update and rename README.md to Tipe_Data.java

---
+++ Tipe_Data.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pertemuan2;
+
+/**
+ *
+ * @author SPC LIFE 5
+ */
+public class Tipe_Data {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String Nama = "Mochammad Tammim D. F.";
+        System.out.println("Nama        : " +Nama);  
+        //tipe data
+        // petik 2(") tipe data string, petik 1(') untuk tipe data char
+        int Umur = 20;
+        double BeratBadan = 48.5;
+        String Prodi = "ILMU KOMPUTER";
+        char Kelas = 'A';
+        boolean LakiLaki = true;
+        
+        System.out.println("Umur        : "   +Umur);
+        System.out.println("Berat Badan : "   +BeratBadan);
+        System.out.println("Prodi       : "    +Prodi);
+        System.out.println("Kelas       : "   +Kelas);
+        System.out.println("Laki-Laki   ? "    +LakiLaki);
+    }
+    
+}
